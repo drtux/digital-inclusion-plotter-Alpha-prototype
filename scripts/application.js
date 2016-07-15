1713fc7f4df5027ed81f2cc88b306e31063dcf69
drtux <benjaminwalker@HOD0048.local>
2016-07-15 17:52:29 +0100

    Merge branch 'master' into Content
    # Conflicts:
    #	index.html
    #	scripts/application.js

--- scripts/application.js
+++ scripts/application.js
@@ -1,24 +1,9 @@
-var skillStack = [];
-
-var skills = [
-/*0*/	{question:"Can they use a search engine?",
-		 hint:"finding something new, help with a problem, somewhere to shop, somewhere to visit, contact details off something"},
-
-/*1*/	{question:"Can they send a email or instant message?",
-		 hint:"to keep in contact with family, or friends, using Outlook, Gmail, WhatsApp, Facebook messenger"},
-
-/*2*/	{question:"Can they buy something online?",
-		 hint:"weekly shopping, general purchases, bargains/offers, specialist goods"},
-
-/*3*/	{question:"Can they fill out a form with personal details?",
-		 hint:"online banking, set up online accounts i.e social media, applications for government services"},
-]
+var helpState = -1;//Retain between questions
 
 var questions = [
 /*0*/	{question:"Have they ever used the internet?",
 		//  hint:"",
-		 yDest:1, nDest:2},
-
+		 yDest:1, nDest:-1},
 
 /*1*/	{question:"Do they still use the internet?",
 		//  hint:"",
@@ -26,7 +11,8 @@ var questions = [
 
 /*2*/	{question:"Do they want to be online?",
 		 hint:"There are barriers stopping them being online, for example it's too expensive, they lack of confidence or aren't physically able",
-		 yDest:-3, nDest:5},
+		 yDest:-3, nDest:-2},
+
 		 //If no to 0 and 2 = Never have, never will
 		 //If yes to 0, then no to 1 and 2 = Was online, but no longer
 		 //Yes = Willing but unable
@@ -41,53 +27,27 @@ var questions = [
 		//Often = Learning the ropes
 		//Only with new or unfamiliar tasks = 5
 		//Rarely or never = 5
+		 yDest:0, nDest:0},
 
-		 yDest:-5, nDest:5},
-
-/*5*/	{question:"Can they:",
-			//<ol> list
-			//use email
-			//use search engines
-			//set up an account (eg an online bank account)
-			//buy things online
+/*5*/	{question:"Can they:<ul class='list list-bullet'><li>use email</li><li>use search engines</li><li>set up an account (for example, an online bank account)</li><li>buy things online</li></ul>",
 		 hint:"",
 		 //Yes = if 5 = 'only with new tasks' then Basic digital
 		 //No, they'd need help
-		 yDest:6, nDest:-5},
-		 //Y =
+		 yDest:0, nDest:0},
 
 /*6*/	{question:"Do they have advanced digital skills?",
 		 hint:"They work in tech, can code or have specialist digital knowledge",
-		 yDest:-9, nDest:-8},
+		 yDest:-9, nDest:-8}
 		 //Y = Expert
 		 //N = Confident
-
-// /*7*/	{question:"Do they still use the internet?",
-// 		 hint:"is actively using the internet currently, and will continue to do so in future",
-// 		 yDest:8, nDest:-2},
-//
-// /*8*/	{question:"Do they see how the internet has benefited them?",
-// 		 hint:"\"yes, and if I had more skill the sky would be the limit\"",
-// 		 yDest:-6, nDest:-4},
-//
-// /*9*/	{question:"Do they enjoy to using the internet?",
-// 		 hint:"uses the internet recreationally - social media, videos/streaming, news, shopping",
-// 		 yDest:-3, nDest:6}
 ];
 
-
 function eval(name, value){
+
 
 	$(".question :input").each(function(index){//Delete any un-needed questions (to account for changing minds)
 		if(parseInt($(this).attr('name').charAt(0)) > parseInt(name.charAt(0))){
 			$(this).parent().parent().remove();
-			for (var i=0;i<skillStack.length; i++)
-			{
-				if (skillStack[i].name === name){
-					skillStack.splice(i,1);//Remove the exsisting answer
-					break;//as it will only appear once
-				}
-			}
 		}
 	});
 	$(".level").each(function(index){
@@ -100,26 +60,36 @@ function eval(name, value){
 
 	switch (parseInt(name.charAt(0)))
 	{
-		case 2://Skills question
-			skillStack.push({name:name, value:value});
-			if (skillStack.length == skills.length)
-			{//If all the skills Q's have been answered evaluate them
-				var yes = 0;
-				for (var i = 0; i < skillStack.length; i++)
-				{
-					if (skillStack[i].value == -1)
-					{
-						yes++;
-					}
-				}
-				if (yes == skillStack.length)
-				{//Yes (all)
-					n = questions[parseInt(name.charAt(0))].yDest;
-				}
-				else
-				{//No (sub-set)
-					n = questions[parseInt(name.charAt(0))].nDest;
-				}
+		case 4://Getting others help question
+			switch(value)
+			{
+				case 1: //Regularly
+					n = -5;
+					break;
+				case 2: //New
+					helpState = 1;
+					n = 5;//skills question
+					break;
+				default: //Never
+					helpState = 0;
+					n = 5;//skills question
+					break;
+
+			}
+			break;
+
+		case 5://Skills question
+			if (value == 1 && helpState == 1)
+			{
+				n = -7;
+			}
+			else if (value == 1 && helpState == 0)
+			{
+				n = 6;
+			}
+			else
+			{//No (sub-set)
+				n = -6;
 			}
 			break;
 
@@ -150,17 +120,17 @@ function eval(name, value){
 
 function appendQuestion(n, question, hint){
 	var html = " ";
-	if (n == 2)
-	{//Skill question
-		for (var i = 0; i < skills.length; i++){
-			html += "<fieldset class='form-group inline question'><legend class='form-label-bold' for='2:"+i+"'>"+skills[i].question+"</legend><p class='form-hint'>E.g. "+skills[i].hint+"</p><label class='block-label'><input id='2:"+i+"Y' name='2:"+i+"' type='radio' data-storage='-1'/>Yes</label><label class='block-label'><input id='2:"+i+"N' name='2:"+i+"' type='radio' data-storage='-2'/>No</label></fieldset>";
-		}
+	switch(n)
+	{
+		case 4://Help question
+			html = "<fieldset class='form-group inline question' name='set"+n+"'><legend class='form-label-bold' for='"+n+"'>"+question+"</legend><p class='form-hint'>For example, "+hint+"</p><label class='block-label'><input id='"+n+"R' name='"+n+"' type='radio' data-storage='1'/>Reguarly</label><label class='block-label'><input id='"+n+"T' name='"+n+"' type='radio' data-storage='2'/>Only new tasks</label><label class='block-label'><input id='"+n+"N' name='"+n+"' type='radio' data-storage='3'/>Rarely/Never</label></fieldset>"
+			break;
+
+		default://Normal questions
+			html = "<fieldset class='form-group inline question' name='set"+n+"'><legend class='form-label-bold' for='"+n+"'>"+question+"</legend><p class='form-hint'> For example, "+hint+"</p><label class='block-label'><input id='"+n+"Y' name='"+n+"' type='radio' data-storage='1'/>Yes</label><label class='block-label'><input id='"+n+"N' name='"+n+"' type='radio' value='0'/>No</label></fieldset>";
+			break;
 	}
-	else
-	{//Normal questions
-		html = "<fieldset class='form-group inline question' name='set"+n+"'><legend class='form-label-bold' for='"+n+"'>"+question+"</legend><p class='form-hint'> E.g. "+hint+"</p><label class='block-label'><input id='"+n+"Y' name='"+n+"' type='radio' data-storage='1'/>Yes</label><label class='block-label'><input id='"+n+"N' name='"+n+"' type='radio' value='0'/>No</label></fieldset>";
-	}
-	$('.form').append(html)
+	$('.form').append(html);
 	var latestElement = document.getElementsByName('set'+n);
 	latestElement[0].scrollIntoView();
 }
@@ -171,20 +141,19 @@ function appendLevel(level, name){
 	var colour = '';
 
 	switch(level){
-		case -1: plot += '1:</br>Never have, never will'; colour = '#DA7357'; break;
-		case -2: plot += '2:</br>Was online but no longer'; colour = '#EA8C5C'; break;
-		case -3: plot += '3:</br>Willing and unable'; colour = '#EC9E5A'; break;
-		case -4: plot += '4:</br>Reluctantly online'; colour = '#F4C15B'; break;
-		case -5: plot += '5:</br>Learning the ropes'; colour = '#F9D45E'; break;
-		case -6: plot += '6:</br>Task specific'; colour = '#EAE05F'; break;
-		case -7: plot += '7:</br>Basic digital skills'; colour = '#D6DA5D'; break;
-		case -8: plot += '8:</br>Confident'; colour = '#B0CC5B'; break;
-		case -9: plot += '9:</br>Expert'; colour = '#9CC55A'; break;
+		case -1: plot += '1:</br>Never have, never will</p><p>They may feel they have ‘missed the boat’ and that learning how to use the internet doesn\'t fit into their lives.'; colour = '#DA7357'; break;
+		case -2: plot += '2:</br>Was online but no longer</p><p>They might have lost trust in the internet. They might be afraid of fraud or seeing inappropriate things online. They might have lost internet access because of cost or physical or mental capability.'; colour = '#EA8C5C'; break;
+		case -3: plot += '3:</br>Willing and unable</p><p>People in this category predominantly have a positive perception of being online but have problems with low skills and they struggle to learn.'; colour = '#EC9E5A'; break;
+		case -4: plot += '4:</br>Reluctantly online</p><p>They understand the general benefits of being online, they have yet to experience them personally.'; colour = '#F4C15B'; break;
+		case -5: plot += '5:</br>Learning the ropes</p><p>These users are predominantly very positive about the benefits of the internet and have willingly started to engage with digital technologies.'; colour = '#F9D45E'; break;
+		case -6: plot += '6:</br>Task specific</p><p>Their tasks may include online banking or updating social media. These tasks are often limited and specific.'; colour = '#EAE05F'; break;
+		case -7: plot += '7:</br>Basic digital skills</p><p>These people have enough skills to be able to navigate online independently and perform all tasks at a basic level.'; colour = '#D6DA5D'; break;
+		case -8: plot += '8:</br>Confident</p><p>Confident users make use of digital tools at work and in their everyday lives.'; colour = '#B0CC5B'; break;
+		case -9: plot += '9:</br>Expert</p><p>Expert internet users have advanced digital skills.'; colour = '#9CC55A'; break;
 	}
 	var html = "<div class='level' name='"+name+"'><p class='heading-large panel-indent' style='background-color: "+colour+";'>They are a category " + plot + "</p> <a class='button' href='routing.html'>Continue</a></div>";
 	$('.form').append(html);
 }
-
 
 
 
@@ -203,6 +172,8 @@ $( document ).ready(function() {
     var file = $(this).attr("data-includefile");
     $(this).load("includes/"+$(this).attr("data-includefile")+".html", unWrapPlaceholder)
   });
+
+  appendQuestion(0, questions[0].question, questions[0].hint);//Add first question
 
   //write to local storage
   //$('form').storeForm();
